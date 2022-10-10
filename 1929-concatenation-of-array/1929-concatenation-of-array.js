9aa0ac6efb9d97573174e4122d3cc619935eb758
Seunmul <87626380+Seunmul@users.noreply.github.com>
2022-10-10 11:17:12 +0900

    Time: 101 ms (78.37%), Space: 46.2 MB (20.20%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -3,19 +3,14 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    if (nums.length<1 && nums.lenght>1000){
-      return null
-  }
-    nums.forEach((item,index)=>{
+
+  nums.forEach((item,index)=>{
         if(item<1 && item>1000)
             return
+        else if(index>1000)
+            return
     })
-    
-//   for (let i=0;i<num.length;i++){
-//     if(nums[i]<1 && nums[i]>1000){
-//         return null
-//     }
-//   }
+
   const newNums = [...nums,...nums]
      return newNums
 };
